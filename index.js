7e3036aed217bd99070ce7aa006f038151d966c3
ufuoma iboyi <ufuomaiboyi23@yahoo.com>
2023-11-13 00:23:44 -1100

    express framework created

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const http= require('http');
+const expresApp= require('./express')
 
 const port= 5000;
 const host = 'localhost';
 
-const server=http.createServer();
+const server=http.createServer(expresApp);
 
 server.listen(port,host,()=>{
     console.log(`server is listening on ${port}`);
